7ec4f294690d449178f5a0c614d42e3c6884ce86
Johnson Joseph <jhonsonj88@gmail.com>
2019-11-04 05:35:46 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -39,3 +39,4 @@ function render() {
 // use your createStore function and the functions provided here to create a store
 // once the store is created, call an initial dispatch
 let store = createStore();
+store.dispatch 
