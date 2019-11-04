9cd319602016a571c0c8e4030fe7e32fe6e30bbc
Johnson Joseph <jhonsonj88@gmail.com>
2019-11-04 05:35:03 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -38,3 +38,4 @@ function render() {
 
 // use your createStore function and the functions provided here to create a store
 // once the store is created, call an initial dispatch
+let store = createStore();
